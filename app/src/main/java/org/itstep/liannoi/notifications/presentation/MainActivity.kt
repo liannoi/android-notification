ad9584fa60b6e68957fd10b9ec2f1a2337e92180
Maksym Liannoi <liannoi444@gmail.com>
2020-07-15 20:55:02 +0300

    Initial commit

---
+++ app/src/main/java/org/itstep/liannoi/notifications/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.itstep.liannoi.notifications.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.itstep.liannoi.notifications.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
